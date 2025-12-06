aeefce31992a3066699299d6e687d18f2167a681
mellamoeterno <richmyself8@gmail.com>
2025-12-06 17:48:03 -0300

    counterComponent

---
+++ src/app/components/counterComponent.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+}
