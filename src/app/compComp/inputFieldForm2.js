bf4a27e3c50e450cb35a90ecb003da160372b2e6
mellamoeterno <richmyself8@gmail.com>
2026-01-08 16:42:47 -0300

    newNotes

--- src/app/compComp/inputFieldForm2.js
+++ src/app/compComp/inputFieldForm2.js
@@ -27,7 +27,7 @@ export default function SimpleForm(){
             ...prev,
             [name]:value,
         }));
-        //about prevvvvvv
+        //about prev
         //prev is used exclusively with state update functions in React when you use the functional update pattern.
         //
         //
