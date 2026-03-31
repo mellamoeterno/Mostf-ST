130a69cbe1b9f455f265f3111c57e93061445a36
mellamoeterno <richmyself8@gmail.com>
2026-03-30 21:51:15 -0300

    nw

---
+++ src/app/comComp+2/things1.js
@@ -0,0 +1,23 @@
+//Why are keys critical in React lists?
+
+//When React re-renders a list, it needs to answer:
+//“Which item is the same as before, which changed, which moved, and which is new or removed?”
+
+/* {items.map((item, index) => (
+  <li key={index}>{item.name}</li>
+))}
+ */
+
+//with proper keys
+
+/* {items.map(item => (
+  <li key={item.id}>{item.name}</li>
+))}
+ */
+
+//“Keys give each div or list item a unique ID, so React tracks changes efficiently. Without them, mapping an array could re-render everything, 
+//breaking form validation or slowing the app.”
+
+
+//this above gives two codes, but the first (bad example) doesnt show without keys, still uses it, figure out wth is a better answer cus this 
+//suckss
